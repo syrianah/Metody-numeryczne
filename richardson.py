38c38d8ec589b793d16becbe72b781d7602b4b6a
BuildTools <unconfigured@null.spigotmc.org>
2019-05-26 18:46:18 +0200

    richardson

--- richardson.py
+++ richardson.py
@@ -1,45 +1,30 @@
 import numpy as np
 
+class Richardson:
+    def __init__(self, A, b):
+        self.maxiter = 10
+        self.delta = 10**-10
+        self.epsilon = 1/2*10**-4
+        self.A = A
+        self.b = b
+
+    def calculate(self):
+        x = np.array([0, 0, 0])
+        n = len(x)
+        Q = np.identity(n)
+        for k in range(self.maxiter):
+            y = x
+            c = (Q - self.A) * x + self.b
+            x = c * np.linalg.inv(Q)
+            print(k, x)
+            if np.linalg.norm(x - y) < self.epsilon:
+                print("convergence")
+
 Q = np.array([[1, 9, 1],
             [4, 1, -1],
             [1, -3, 12]])
 
-I = np.array([[1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1]])
-
-L = np.array([[0, 0, 0],
-            [4, 0, 0],
-            [1, -3, 0]])
-
-D = np.array([[1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 12]])
-
-U = np.array([[0, 9, 1],
-            [0, 0, -1],
-            [0, 0, 0]])
-
-x = np.array([[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]])
-
-# x = np.array([[0], [0], [0]])
-
 b = np.array([5, 3, 31])
 
-A = L + D + U
-
-R = A - D
-
-print(A)
-
-# x = np.zeros(3)
-
-# sum = np.dot(A, x)
-# print(sum)
-
-for i in range(2):
-    x[i+1] = x[i] - np.dot(A, x[i]) + b[i]
-
-print(x)
+x = Richardson(Q, b)
+x.calculate()
